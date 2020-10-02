775fa8b711240555c6c241ee10ffcb72ad40a92b
Arindam Bhattacharya <arinbhat91@gmail.com>
2020-10-02 22:02:00 +0530

    Added custom user:

---
+++ app/core/tests/test_model.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """ Test creating new user with and email is successfull"""
+        email = 'arinbhat91@gmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalize(self):
+        """ new user email normalised """
+        email = 'arinbhat91@yahoo.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test creating for no email reise error """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
